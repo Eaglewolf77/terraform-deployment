aa3cffd4097e3654cd13e5a09f72bcbd92290a75
Erik Ullström <erikullstrom@Eritzy.local>
2025-05-14 21:44:09 +0200

    final terraform fix: remove deprecated backend_address_pools_ids

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
@@ -14,8 +23,8 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "tf-test-subnet"
+resource "azurerm_subnet" "web_subnet" {
+  name                 = "tf-web-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -25,9 +34,38 @@ resource "azurerm_network_security_group" "nsg" {
   name                = "tf-test-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-resource "azurerm_public_ip" "lb_ip" {
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.web_subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "web_ip" {
   name                = "tf-web-lb-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -35,21 +73,20 @@ resource "azurerm_public_ip" "lb_ip" {
   sku                 = "Standard"
 }
 
-resource "azurerm_lb" "web_lb" {
+resource "azurerm_lb" "lb" {
   name                = "tf-web-lb"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
-
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.lb_ip.id
+    public_ip_address_id = azurerm_public_ip.web_ip.id
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "backend_pool" {
-  loadbalancer_id = azurerm_lb.web_lb.id
-  name            = "backend-pool"
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "BackendPool"
 }
 
 resource "azurerm_network_interface" "web_nic" {
@@ -59,21 +96,33 @@ resource "azurerm_network_interface" "web_nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.web_subnet.id
     private_ip_address_allocation = "Dynamic"
-    load_balancer_backend_address_pools_ids = [azurerm_lb_backend_address_pool.backend_pool.id]
+    public_ip_address_id          = azurerm_public_ip.web_ip.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "webserver" {
+resource "azurerm_network_interface_backend_address_pool_association" "nic_assoc" {
+  network_interface_id    = azurerm_network_interface.web_nic.id
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backend_pool.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
   name                = "tf-webserver"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1ls"
+  size                = "Standard_B1s"
   admin_username      = "azureuser"
+
   network_interface_ids = [
     azurerm_network_interface.web_nic.id,
   ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = var.ssh_public_key
+  }
 
   os_disk {
     caching              = "ReadWrite"
@@ -82,29 +131,8 @@ resource "azurerm_linux_virtual_machine" "webserver" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
     version   = "latest"
   }
-
-  disable_password_authentication = true
 }
-
-resource "azurerm_automation_account" "automation" {
-  name                = "tf-automation"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "Basic"
-}
-
-resource "azurerm_key_vault" "kv" {
-  name                        = "keyvault-kv-cloud23"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-}
-
-data "azurerm_client_config" "current" {}
