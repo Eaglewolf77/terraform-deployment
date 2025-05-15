98716114f82fdae9b50d4f44034c11d1e65ca5a6
Erik Ullström <erikullstrom@Eritzy.local>
2025-05-15 12:19:37 +0200

    skapar backend för att kunna köra terraform-destroy

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -2,13 +2,32 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.117"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tf-backend-rg"
+    storage_account_name = "tfbackendstorageaccount"
+    container_name       = "tfstate"
+    key                  = "terraform-infra.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+variable "ssh_public_key" {
+  description = "SSH public key to store in Key Vault"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -16,123 +35,4 @@ resource "azurerm_resource_group" "rg" {
   location = "swedencentral"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "tf-test-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "web_subnet" {
-  name                 = "tf-web-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "tf-test-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
-  subnet_id                 = azurerm_subnet.web_subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_public_ip" "web_ip" {
-  name                = "tf-web-lb-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_lb" "lb" {
-  name                = "tf-web-lb"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard"
-
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.web_ip.id
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "backend_pool" {
-  loadbalancer_id = azurerm_lb.lb.id
-  name            = "BackendPool"
-}
-
-resource "azurerm_network_interface" "web_nic" {
-  name                = "tf-webserver-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.web_subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_interface_backend_address_pool_association" "nic_assoc" {
-  network_interface_id    = azurerm_network_interface.web_nic.id
-  ip_configuration_name   = "internal"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.backend_pool.id
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "tf-webserver"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.web_nic.id,
-  ]
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = var.ssh_public_key
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts-gen2"
-    version   = "latest"
-  }
-}
+/* Här fortsätter din vanliga kod för vNet, NSG, VM osv */
