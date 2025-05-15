5cd9fbc83cb0132b5338c86c1ec756a26f98deb2
Erik Ullström <erikullstrom@Eritzy.local>
2025-05-15 12:50:39 +0200

    skapar backend för att kunna köra terraform-destroy

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -26,10 +26,6 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
 variable "subscription_id" {}
-variable "ssh_public_key" {
-  description = "SSH public key to store in Key Vault"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "tf-test-rg"
   location = "swedencentral"
