5d21c3d70d651d7e20a46fd641a6224c49d14b4e
Erik Ullström <erikullstrom@Eritzy.local>
2025-05-16 16:24:39 +0200

    fix: prevent duplicate keyvault secret apply

--- terraform-secrets/main.tf
+++ terraform-secrets/main.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
 
 data "azurerm_key_vault" "existing" {
@@ -18,11 +14,8 @@ resource "azurerm_key_vault_secret" "ssh_key" {
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes  = [value]
+    ignore_changes = [
+      value
+    ]
   }
 }
-
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
