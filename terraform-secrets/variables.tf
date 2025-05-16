921fab163dbdff27d630b12943db799a059e20c6
Erik Ullström <erikullstrom@Eritzy.local>
2025-05-16 12:24:07 +0200

    uppdatera med input false

--- terraform-secrets/variables.tf
+++ terraform-secrets/variables.tf
@@ -2,3 +2,6 @@ variable "ssh_public_key" {
   description = "SSH public key to store in Key Vault"
   type        = string
 }
+variable "client_object_id" {
+  description = "Object ID for the Service Principal"
+}
